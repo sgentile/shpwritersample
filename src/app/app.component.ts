67c27016868a37466183f11e254181addcb826b6
Steven Gentile <steven.gentile@gmail.com>
2017-11-20 23:56:33 -0500

    sample shp-writer example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import * as shpwrite from 'shp-write';
+import * as json from '../assets/sample.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'app';
+  inputText = '';
+  fileName = 'Sample';
+
+  createShapeFile() {
+    var options = {
+      folder: this.fileName,
+      types: {
+          point: this.fileName,
+          polygon: this.fileName,
+          line: this.fileName,
+      }
+    }
+    const jsonString = JSON.parse(this.inputText);
+    shpwrite.download(jsonString, options);
+  }
+
+
+  ngOnInit() {
+      this.inputText = JSON.stringify(json, undefined, 4);
+  }
 }
